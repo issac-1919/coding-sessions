46b7f5937036442421a82e753a6c282b678b2118
issac-1919 <krisissacsamuel@gmail.com>
2023-02-20 14:33:46 +0530

    bit manipulation left and right shifts

---
+++ bit_manipulation.py
@@ -0,0 +1,11 @@
+def left_shift_():
+    a = 2
+    for i in range(1, 11):
+        # decreases
+        print(a<<i)
+
+def right_shift_():
+    a = 1000
+    for i in range(1,11):
+        # increases
+        print(a>>i)
